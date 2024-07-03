74995b1420a9dacfdafeeef89557daca5d664e8f
Samanta Driuzzi <driuzzi.samanta.mayra@gmail.com>
2024-07-03 13:46:21 -0300

    ajustes en la inicializacion del server

--- src/index.ts
+++ src/index.ts
@@ -4,10 +4,26 @@ import "reflect-metadata";
 import { AppDataSource } from "./config/data-source";
 import { preLoadData } from "./helpers/preloadData";
 
-AppDataSource.initialize().then((res) => {
-  console.log("Conectada a la base de datos correctamente");
+// AppDataSource.initialize().then((res) => {
+//   console.log("Conectada a la base de datos correctamente");
+//
+//   server.listen(PORT, () => {
+//     console.log(`Server listening on port ${PORT}`);
+//   });
+// });
 
+const inicialize = async () => {
+  console.log("Iniatializing...");
+
+  const databaseUrl = process.env.DATABASE_URL;
+  AppDataSource.setOptions({
+    url: databaseUrl,
+    ssl: { rejectUnauthorized: false },
+  });
+  await AppDataSource.initialize();
+  console.log("Conectada a la base de datos correctamente");
+  await preLoadData();
   server.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}`);
   });
-});
+};
